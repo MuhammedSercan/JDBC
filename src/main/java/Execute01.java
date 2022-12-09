3d3c695d48f3923e8703cfa327694c31a7090951
MuhammedSercan <msercanustfdm@gmail.com>
2022-12-09 16:20:29 +0300

    v2

--- src/main/java/Execute01.java
+++ src/main/java/Execute01.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 public class Execute01 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
+
         Class.forName("org.postgresql.Driver");
 
         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed","postgres","Sercan91$");
